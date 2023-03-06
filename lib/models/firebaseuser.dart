ecf2df8f66cfba2491d73fc9cc04b9de637fa94c
Wasin Wasantivong <chotongza@gmail.com>
2023-03-07 02:34:23 +0700

    delete model firbaseuser

--- lib/models/firebaseuser.dart
+++
@@ -1,9 +0,0 @@
-class FirebaseUser {
-  final String? uid;
-  final String? code; //code firebaseauth excemption
-  FirebaseUser({this.uid, this.code});
-
-  String? getUid() {
-    return uid;
-  }
-}
