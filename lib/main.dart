96f9ee65930309c6e66f0f24e4db91595278fc44
Wasin Wasantivong <chotongza@gmail.com>
2023-03-08 00:54:45 +0700

    edit comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:project_mobile_app/inventory.dart';
 import 'firebase_options.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 
 import 'screens/authenticate/Login.dart';
@@ -64,90 +62,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
-// class MyHomePage extends StatelessWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   final String title;
-
-//   void _logout(BuildContext context) async {
-//     try {
-//       // Sign out the current user using Firebase Authentication
-//       final auth = FirebaseAuth.instance;
-//       await auth.signOut();
-
-//       // Navigate to the login screen after a successful logout
-//       Navigator.pushReplacementNamed(context, '/login');
-//     } on FirebaseAuthException catch (e) {
-//       // Show an error message if the logout was unsuccessful
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(
-//           content: Text(e.toString()),
-//         ),
-//       );
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(title),
-//       ),
-//       body: const Center(
-//         child: Text('My Page!'),
-//       ),
-//       drawer: Drawer(
-//         child: ListView(
-//           padding: EdgeInsets.zero,
-//           children: [
-//             const UserAccountsDrawerHeader(
-//               // <-- SEE HERE
-//               decoration: BoxDecoration(color: const Color(0xff764abc)),
-//               accountName: Text(
-//                 "jiw jiw local store",
-//                 style: TextStyle(
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//               accountEmail: Text(
-//                 "jiwjiw.store@gmail.com",
-//                 style: TextStyle(
-//                   fontWeight: FontWeight.bold,
-//                 ),
-//               ),
-//               currentAccountPicture: FlutterLogo(),
-//             ),
-//             Card(
-//               child: ListTile(
-//                 title: const Text('Dash borad'),
-//                 leading: Icon(Icons.more_vert),
-//                 onTap: () {
-//                   Navigator.pop(context);
-//                 },
-//               ),
-//             ),
-//             Card(
-//               child: ListTile(
-//                 title: const Text('Inventory'),
-//                 leading: Icon(Icons.more_vert),
-//                 onTap: () {
-//                   Navigator.pop(context);
-//                 },
-//               ),
-//             ),
-//             Card(
-//               child: ListTile(
-//                 title: const Text('Sell'),
-//                 leading: Icon(Icons.more_vert),
-//                 onTap: () {
-//                   Navigator.pop(context);
-//                 },
-//               ),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
