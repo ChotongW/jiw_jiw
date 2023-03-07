a5b5b0db32813d3a874a8da38242ad80a83b8ecf
Wasin Wasantivong <chotongza@gmail.com>
2023-03-08 00:54:59 +0700

    add new item_list file

---
+++ lib/item/list_item.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class itemList extends StatefulWidget {
+  const itemList({super.key});
+
+  @override
+  State<itemList> createState() => _itemListState();
+}
+
+class _itemListState extends State<itemList> {
+  @override
+  final List<String> myProducts = [
+    'Drink',
+    'Fresh meal',
+    'Snacks',
+    'Frozen & Processed Food',
+    'Pets',
+    'Household Goods',
+    'Shower',
+    'Mom and kids',
+    'Fresh Product',
+  ];
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Category '),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        // implement GridView.builder
+        child: GridView.builder(
+            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 500,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20),
+            itemCount: myProducts.length,
+            itemBuilder: (BuildContext ctx, index) {
+              return Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30)),
+                child: InkWell(
+                  onTap: () {},
+                  child: Center(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.search,
+                        size: 40,
+                      ),
+                      Text(myProducts[index])
+                    ],
+                  )),
+                ),
+              );
+            }),
+      ),
+    );
+  }
+}
