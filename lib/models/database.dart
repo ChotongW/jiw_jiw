3571f75bfdb3b166024acf1b128d17f1df025afb
Wasin Wasantivong <chotongza@gmail.com>
2023-03-08 05:03:41 +0700

    create database model and method

---
+++ lib/models/database.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_database/firebase_database.dart';
+
+class DatabaseService {
+  final FirebaseDatabase database = FirebaseDatabase.instance;
+
+  void write({
+    required String userId,
+    required Map<String, dynamic> data,
+  }) async {
+    try {
+      // print(userId);
+      // print(data);
+      DatabaseReference _databaseReference = database.ref("users/$userId");
+
+      await _databaseReference.set(data);
+    } catch (e) {
+      print(e);
+      rethrow;
+    }
+  }
+
+  Future<String> read({required String userId}) async {
+    try {
+      DatabaseReference _databaseReference = database.ref("users/$userId");
+      final snapshot = await _databaseReference.get();
+      if (snapshot.exists) {
+        Map<String, dynamic> _snapshotValue =
+            Map<String, dynamic>.from(snapshot.value as Map);
+        return _snapshotValue['name'] ?? '';
+      } else {
+        return '';
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
