7106370df9366c1aa7b879eaebfeb9bc9d8c1646
Mrpopopopo <113658345+Mrpopopopo@users.noreply.github.com>
2023-03-10 00:51:05 +0700

    Do search

--- lib/screens/item/search.dart
+++ lib/screens/item/search.dart
@@ -1,91 +1,36 @@
 import 'package:flutter/material.dart';
 
-class Search extends StatelessWidget {
+List<dynamic> itemmock = [
+  {"name": "banana", "price": "30", "quantity": "10"},
+  {"name": "Apple", "price": "20", "quantity": "10"}
+];
+
+class Search extends StatefulWidget {
   const Search({super.key});
 
   @override
+  State<Search> createState() => _SearchState();
+}
+
+class _SearchState extends State<Search> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('search'),
-      ),
-      body: SafeArea(
-          child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            height: 60,
-            width: 130,
-            decoration: BoxDecoration(
-                color: Color(0xffFFDDFF),
-                border: Border.all(color: Colors.black, width: 3),
-                borderRadius: BorderRadius.circular(10)),
-            child: Text('Search Item', style: TextStyle(fontSize: 15.6)),
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: Color(0xffDBEAFB),
-                borderRadius: BorderRadius.circular(12)),
-            padding: EdgeInsets.all(12),
-            child: Row(
-              children: [
-                Icon(Icons.search),
-                Text(
-                  'Search',
-                  style: TextStyle(color: Color(0xffB3A7B7)),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(20),
-            margin: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Color(0xffF085CB),
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Image.asset(
-                  "images/img1.jpg",
-                  width: 50,
-                  height: 50,
-                ),
-                Text(
-                  "Fruit",
-                  style: TextStyle(fontSize: 23),
-                )
-              ],
-            ),
-          ),
-          Row(
-            children: [
-              Expanded(
-                  child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.all(5)),
-                  Row(
-                    children: [Text('Item Name')],
-                    mainAxisAlignment: MainAxisAlignment.center,
-                  )
-                ],
-              )),
-              Expanded(
-                  child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.all(5)),
-                  Row(
-                    children: [Text('Price/unit')],
-                    mainAxisAlignment: MainAxisAlignment.center,
-                  )
-                ],
-              ))
-            ],
-          )
-        ],
-      )),
-    );
+        appBar: AppBar(
+          title: Text('SearchItem'),
+        ),
+        body: ListView.builder(
+          itemCount: itemmock.length,
+          itemBuilder: (context, index) {
+            return Card(
+              child: ListTile(
+                title: Text(itemmock[index]['name']),
+                subtitle: Text(itemmock[index]['quantity']),
+                trailing: Text(itemmock[index]['price']),
+                onTap: () {},
+              ),
+            );
+          },
+        ));
   }
 }
