b994ba65814088783f622390f56c56edc7a6894f
Wasin Wasantivong <chotongza@gmail.com>
2023-03-09 22:18:52 +0700

    remove unused import

--- lib/screens/item/list_item.dart
+++ lib/screens/item/list_item.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:project_mobile_app/screens/item/search.dart';
 
 class itemList extends StatefulWidget {
   const itemList({super.key});
