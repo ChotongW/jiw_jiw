b1d54d53f554de8b7e941647ff2b69139971a207
Wasin Wasantivong <chotongza@gmail.com>
2023-03-19 05:40:30 +0700

    create searchFor del

---
+++ lib/screens/item/searchForDel.dart
@@ -0,0 +1,184 @@
+import 'package:flutter/material.dart';
+
+import '../../models/database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import '../../services/auth.dart';
+
+List<dynamic> itemmock = [
+  {"name": "banana", "price": "30", "quantity": "10"},
+  {"name": "Apple", "price": "20", "quantity": "10"}
+];
+
+class SearchForDel extends StatefulWidget {
+  const SearchForDel({super.key, required this.category});
+  final String category;
+  @override
+  State<SearchForDel> createState() => _SearchForDelState();
+}
+
+class _SearchForDelState extends State<SearchForDel> {
+  final DatabaseService _db = DatabaseService();
+  final AuthService _auth = AuthService();
+  int _counter = 1;
+  List<dynamic> listData = [];
+
+  Future<void> getData() async {
+    List<dynamic>? listRes = [];
+    User? result = await _auth.currentUser;
+
+    if (result == null) {
+      //null means unsuccessfull authentication
+      await showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              content: Text('not found current user!'),
+            );
+          });
+      await Navigator.pushReplacementNamed(context, '/auth');
+    } else {
+      // print(item.category);
+      Map? data = await _db.itemsbyCategory(
+        userId: result.uid.toString(),
+        category: widget.category,
+      );
+      // print(data.runtimeType);
+      data?.forEach((key, value) {
+        // print(key);
+        listRes.add(value);
+        value.forEach((key, value) {
+          // print(value);
+        });
+      });
+      // print(listRes);
+      setState(() {
+        // print(listRes);
+        listData = listRes;
+        // print(listData);
+      });
+    }
+
+    // dynamic res = await _db.itemsbyCategory()
+  }
+
+  void initState() {
+    super.initState();
+    // Initialize state variables here
+    getData();
+  }
+
+  @override
+  void didUpdateWidget(SearchForDel oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // Load data based on new dropdown value
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (listData.isEmpty) {
+      // Show a CircularProgressIndicator if the data is still being fetched
+      return Scaffold(
+          body: Center(
+        child: Text('no data found'),
+      ));
+    }
+    return Scaffold(
+        body: ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      // itemCount: listData == null ? 1 : listData.length,
+      itemCount: listData.length,
+      itemBuilder: (context, index) {
+        return Card(
+            child: ListTile(
+          title: Wrap(children: [
+            Text(listData[index]['name']),
+            SizedBox(
+              width: 10,
+            ),
+            Text(listData[index]['quantity'].toString()),
+            SizedBox(
+              width: 1000,
+            ),
+          ]),
+          //subtitle: Text(listData[index]['quantity'].toString()),
+          trailing: Wrap(children: [
+            Text(listData[index]['quantity'].toString()),
+            SizedBox(
+              width: 10,
+            ),
+          ]),
+
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                // Create the AlertDialog widget
+                return StatefulBuilder(
+                    builder: ((context, setState) => AlertDialog(
+                          title: Text(listData[index]['name']),
+                          content: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: <Widget>[
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.remove_circle,
+                                      color: Colors.red,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        if (_counter > 1) {
+                                          _counter--;
+                                        }
+                                      });
+                                    },
+                                  ),
+                                  Text(
+                                    'Current value: $_counter',
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                  IconButton(
+                                    icon: Icon(
+                                      Icons.add_circle,
+                                      color: Colors.green,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        _counter++;
+                                      });
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          actions: <Widget>[
+                            TextButton(
+                              child: Text('Cancle'),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            TextButton(
+                              child: Text('Upadate Item'),
+                              onPressed: () {
+                                // Handle Ok button press
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        )));
+              },
+            );
+          },
+        ));
+      },
+    ));
+  }
+}
