9a5e21ead32afd6ff884522b9b93a325d913d98c
Wasin Wasantivong <chotongza@gmail.com>
2023-03-24 23:09:18 +0700

    comment for AuthService

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,8 +1,27 @@
+// -----------------------------------------------------------------------------
+// auth.dart
+// -----------------------------------------------------------------------------
+//
+// This header file contains functions for efficiently middleware autentication.
+// for easily to use we crate class AuthService.
+
 import 'package:firebase_auth/firebase_auth.dart';
 import '../models/profile.dart';
 
+// -----------------------------------------------------------------------------
+// AuthService
+// -----------------------------------------------------------------------------
+//
+// The AuthService class is create Firebase Authen instance
+// for login and register by email firebase authentication.
+// we use this class as middleware for login and register.
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // currentUser()
+  // this function return instance of User class that contain user data if logged in.
+  // implement User class from firebase auth
+  // if not logged in return null.
 
   get currentUser {
     User? user = _auth.currentUser;
@@ -23,6 +42,13 @@ class AuthService {
   //   return _auth.userChanges();
   // }
 
+  // signInEmailPassword(_login)
+  // this function act as middleware for login.
+  // required _login variable that is instance of Profile class.
+  // implement User class from firebase auth
+  // return instance of User class that contain user data.
+  // if error return error.
+
   Future signInEmailPassword(Profile _login) async {
     try {
       UserCredential userCredential = await _auth.signInWithEmailAndPassword(
@@ -34,6 +60,13 @@ class AuthService {
       return e;
     }
   }
+
+  // registerEmailPassword(_login)
+  // this function act as middleware for register.
+  // required _login variable that is instance of Profile class.
+  // implement User class from firebase auth
+  // return instance of User class that contain user data.
+  // if error return error.
 
   Future registerEmailPassword(Profile _login) async {
     try {
@@ -50,6 +83,12 @@ class AuthService {
       return e;
     }
   }
+
+  // signOut()
+  // this function use for signout.
+  // implement Firebase Auth instance
+  // return state of signout
+  // if error return error.
 
   Future signOut() async {
     try {
